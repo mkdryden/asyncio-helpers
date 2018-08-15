3f8b8f66657878a2f15916e964ce6d695579473f
Christian Fobel <christian@fobel.net>
2018-08-15 15:54:24 -0400

    feat(init): initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# asyncio helpers #
+
+Helper functions, etc. for asyncio development
+
+-------------------------------------------------------------------------------
+
+Install
+-------
+
+The latest [`asyncio-helpers` release][3] is available as a
+[Conda][2] package from the [`sci-bots`][4] channel.
+
+To install `asyncio-helpers` in an **activated Conda environment**, run:
+
+    conda install -c sci-bots -c conda-forge asyncio-helpers
+
+-------------------------------------------------------------------------------
+
+License
+-------
+
+This project is licensed under the terms of the [BSD license](/LICENSE.md)
+
+-------------------------------------------------------------------------------
+
+Contributors
+------------
+
+ - Christian Fobel ([@sci-bots](https://github.com/sci-bots))
+
+
+[1]: https://www.arduino.cc/en/Reference/HomePage
+[2]: http://www.scons.org/
+[3]: https://github.com/sci-bots/asyncio-helpers
---
+++ src/asyncio_helpers/__init__.py
@@ -0,0 +1,146 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from functools import wraps
+import platform
+import threading
+
+from logging_helpers import _L
+import trollius as asyncio
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
+def cancellable(f):
+    '''
+    Decorator to add `started` event attribute and `cancel()` method.
+
+    The `cancel()` method cancels the running coroutine and by raising a
+    `CancelledError` exception.
+    '''
+    started = threading.Event()
+
+    def _wrapped(*args, **kwargs):
+        started.clear()
+        started.loop = ensure_event_loop()
+        started.set()
+        return started.loop.run_until_complete(f(*args, **kwargs))
+
+    def _cancel():
+        loop = ensure_event_loop()
+        tasks = [task for task in asyncio.Task.all_tasks(loop=loop)
+                if task is not
+                asyncio.tasks.Task.current_task(loop=loop)]
+        list(map(lambda task: task.cancel(), tasks))
+
+    def cancel():
+        started.loop.call_soon_threadsafe(_cancel)
+
+    _wrapped.started = started
+    _wrapped.cancel = lambda: started.loop.call_soon_threadsafe(_cancel)
+    return _wrapped
+
+
+def new_file_event_loop():
+    '''
+    Create an asyncio event loop compatible with file IO events, e.g., serial
+    device events.
+
+    On Windows, only sockets are supported by the default event loop (i.e.,
+    :class:`asyncio.SelectorEventLoop`). However, file IO events _are_
+    supported on Windows by the :class:`asyncio.ProactorEventLoop` event loop
+    to support file I/O events.
+
+    Returns
+    -------
+    asyncio.ProactorEventLoop or asyncio.SelectorEventLoop
+    '''
+    return (asyncio.ProactorEventLoop() if platform.system() == 'Windows'
+            else asyncio.new_event_loop())
+
+
+def ensure_event_loop():
+    '''
+    Ensure that an asyncio event loop has been bound to the local thread
+    context.
+
+    .. notes::
+        While an asyncio event loop _is_ assigned to the local thread context
+        for the main execution thread, other threads _do not_ have an event
+        loop bound to the thread context.
+
+    Returns
+    -------
+    asyncio.ProactorEventLoop or asyncio.SelectorEventLoop
+    '''
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError as e:
+        if 'There is no current event loop' in str(e):
+            loop = new_file_event_loop()
+            asyncio.set_event_loop(loop)
+        else:
+            raise
+    return loop
+
+
+def with_loop(func):
+    '''
+    Decorator to run function within an asyncio event loop.
+
+    .. notes::
+        Uses :class:`asyncio.ProactorEventLoop` on Windows to support file I/O
+        events, e.g., serial device events.
+
+        If an event loop is already bound to the thread, but is either a)
+        currently running, or b) *not a :class:`asyncio.ProactorEventLoop`
+        instance*, execute function in a new thread running a new
+        :class:`asyncio.ProactorEventLoop` instance.
+    '''
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        loop = ensure_event_loop()
+
+        thread_required = False
+        if loop.is_running():
+            _L().debug('Event loop is already running.')
+            thread_required = True
+        elif all([platform.system() == 'Windows',
+                  not isinstance(loop, asyncio.ProactorEventLoop)]):
+            _L().debug('`ProactorEventLoop` required, not `%s` loop in '
+                       'background thread.', type(loop))
+            thread_required = True
+
+        if thread_required:
+            _L().debug('Execute new loop in background thread.')
+            finished = threading.Event()
+
+            def _run(generator):
+                loop = ensure_event_loop()
+                try:
+                    result = loop.run_until_complete(asyncio
+                                                     .ensure_future(generator))
+                except Exception as e:
+                    finished.result = None
+                    finished.error = e
+                else:
+                    finished.result = result
+                    finished.error = None
+                finally:
+                    loop.close()
+                    _L().debug('closed event loop')
+                finished.set()
+            thread = threading.Thread(target=_run,
+                                      args=(func(*args, **kwargs), ))
+            thread.daemon = True
+            thread.start()
+            finished.wait()
+            if finished.error is not None:
+                raise finished.error
+            return finished.result
+
+        _L().debug('Execute in exiting event loop in main thread')
+        return loop.run_until_complete(func(**kwargs))
+    return wrapped
